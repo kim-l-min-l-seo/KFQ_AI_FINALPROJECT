3c7225396dc17ca7fd954557c144079d5f1b5dd3
BlackCode <46194003+IIBlackCode@users.noreply.github.com>
2021-11-05 15:29:02 +0900

    터틀봇 마스크 - 정상화

--- WAS/WEB/view/TurtlebotCamera/MaskDetection.py
+++ WAS/WEB/view/TurtlebotCamera/MaskDetection.py
@@ -1,9 +1,12 @@
+import tensorflow as tf
+tf.debugging.set_log_device_placement(True)
 from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
 from tensorflow.keras.models import load_model
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import os
+from tkinter import messagebox
 
 def maskDetection(image):
     model = load_model(os.getcwd()+"\WEB\\" +"view\WebCamera\models\mask_detector.model")
@@ -15,21 +18,16 @@ def maskDetection(image):
     # cap = cv2.VideoCapture(0)
     # ret, img = cap.read()
 
+    # fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
     # out = cv2.VideoWriter('output.mp4', fourcc, image.get(cv2.CAP_PROP_FPS), (image.shape[1], image.shape[0]))
 
+    h, w = image.shape[:2]
+
+    blob = cv2.dnn.blobFromImage(image, scalefactor=1., size=(300, 300), mean=(104., 177., 123.))
+    facenet.setInput(blob)
+    dets = facenet.forward()
+    
     while(True):
-        # ret, img = cap.read()
-        # if not ret:
-            # break
-
-        h, w = image.shape[:2]
-
-        blob = cv2.dnn.blobFromImage(
-            image, scalefactor=1., size=(300, 300), mean=(104., 177., 123.))
-        facenet.setInput(blob)
-        dets = facenet.forward()
-
-        # result_img = image.copy()
 
         for i in range(dets.shape[2]):
             confidence = dets[0, 0, i, 2]
@@ -48,7 +46,8 @@ def maskDetection(image):
                 face_input = preprocess_input(face_input)
                 face_input = np.expand_dims(face_input, axis=0)
             except Exception as e:
-                print(str(e))
+                # print(str(e))
+                pass
             mask, nomask = model.predict(face_input).squeeze()
 
             if mask > nomask:
@@ -57,17 +56,20 @@ def maskDetection(image):
             else:
                 color = (0, 0, 255)
                 label = 'No Mask %d%%' % (nomask * 100)
+                messagebox.showwarning("<경고>","마스크 미착용자가 발견되었습니다.")
 
-            cv2.rectangle(image, pt1=(x1, y1), pt2=(x2, y2),
-                        thickness=2, color=color, lineType=cv2.LINE_AA)
-            cv2.putText(image, text=label, org=(x1, y1 - 10), fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                        fontScale=0.8, color=color, thickness=2, lineType=cv2.LINE_AA)
-            # print(x1, y1, x2, y2)
+            cv2.rectangle(image, pt1=(x1, y1), 
+                        pt2=(x2, y2),thickness=2, 
+                        color=color, 
+                        lineType=cv2.LINE_AA)
+            cv2.putText(image, text=label, 
+                        org=(x1, y1 - 10), 
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                        fontScale=0.8, color=color, 
+                        thickness=2, lineType=cv2.LINE_AA)
+
+            if label == 'No Mask':
+                print("No MASK")
+
         return image
-        # out.write(image)
-        # cv2.imshow('result', image)
-        # if cv2.waitKey(1) == ord('q'):
-            # break
 
-    # out.release()
-    # cap.release()
